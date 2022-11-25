6a8faaa3a027268e57a940ef1fa7bd0f02abe6da
cqc-melf <70640934+cqc-melf@users.noreply.github.com>
2022-11-25 08:22:07 +0000

    update compilation pass and pytket version (#11)
    * update compisation pass and pytket version
    
    * use prerelease
    
    * update testcase
    
    * update testcase
    
    * fix format

--- pytket/extensions/pyquil/backends/forest.py
+++ pytket/extensions/pyquil/backends/forest.py
@@ -134,7 +134,7 @@ class ForestBackend(Backend):
     def rebase_pass(self) -> BasePass:
         return auto_rebase_pass({OpType.CZ, OpType.Rz, OpType.Rx})
 
-    def default_compilation_pass(self, optimisation_level: int = 1) -> BasePass:
+    def default_compilation_pass(self, optimisation_level: int = 2) -> BasePass:
         assert optimisation_level in range(3)
         passlist = [
             DecomposeBoxes(),
@@ -352,7 +352,7 @@ class ForestStateBackend(Backend):
     def rebase_pass(self) -> BasePass:
         return auto_rebase_pass({OpType.CZ, OpType.Rz, OpType.Rx})
 
-    def default_compilation_pass(self, optimisation_level: int = 1) -> BasePass:
+    def default_compilation_pass(self, optimisation_level: int = 2) -> BasePass:
         assert optimisation_level in range(3)
         passlist = [DecomposeBoxes(), FlattenRegisters()]
         if optimisation_level == 1:
