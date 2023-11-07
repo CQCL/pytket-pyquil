5273957494de20bf4471cb7c575e6ce43b9a4af3
Melf <cqc@melf.de>
2023-11-07 13:54:10 +0000

    remove line from test < 10

--- tests/qvm_backend_test.py
+++ tests/qvm_backend_test.py
@@ -439,7 +439,6 @@ def test_gateset_ii(qvm: None, quilc: None) -> None:
     res = forest_backend.get_result(h)
 
     assert res.get_shots().shape == (10, 6)
-    assert res.get_counts()[(0, 0, 0, 0, 0, 0)] < 10
     assert (
         res.get_counts()[(0, 0, 0, 0, 0, 0)] + res.get_counts()[(0, 0, 0, 0, 1, 1)]
     ) == 10
