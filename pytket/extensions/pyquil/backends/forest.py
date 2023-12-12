63e7f1b74307bfdc2bd938370d8865c282f41d8e
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2023-12-12 16:02:01 +0000

    consistent syntax

--- pytket/extensions/pyquil/backends/forest.py
+++ pytket/extensions/pyquil/backends/forest.py
@@ -167,7 +167,7 @@ class ForestBackend(Backend):
         passlist.append(NaivePlacementPass(self.backend_info.architecture))  # type: ignore
         if optimisation_level == 2:
             # Add some connectivity preserving optimisations after routing.
-            passlist.extend([KAKDecomposition(allow_swaps=False), CliffordSimp(False)])
+            passlist.extend([KAKDecomposition(allow_swaps=False), CliffordSimp(allow_swaps=False)])
         if optimisation_level > 0:
             passlist.append(SynthesiseTket())
         passlist.append(self.rebase_pass())
