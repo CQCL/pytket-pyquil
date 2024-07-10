44bcc02cc4795f4ce7870efd578a2267423936d1
cqc-melf <70640934+cqc-melf@users.noreply.github.com>
2024-07-10 17:08:21 +0100

    not Release 0.36 (#92)
    * update pytket
    
    * update changelog
    
    * update version
    
    * add docs link to readme
    
    * add link to slack and stackexchange
    
    * remove publish dosc from workflow
    
    * remove publish docs II
    
    * update changelog

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.27",
+        "pytket >= 1.30.0",
         "pyquil ~= 3.5",
         "typing-extensions ~= 4.2",
     ],
