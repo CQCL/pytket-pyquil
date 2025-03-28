f89c0000c504a2ef1c19abf80824fe5d0040c0f3
Melf <cqc@melf.de>
2025-03-28 14:53:06 +0000

    update pytket version and clean up mypy, fix CI

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket >= 1.39.0",
+        "pytket >= 2.0.1",
         "pyquil >= 4.13.0",
         "typing-extensions >= 4.12.2",
     ],
